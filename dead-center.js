c4736ba99e5059eb7c2fa8341e36137efcee750e
Jacob Scott <jbscottok@gmail.com>
2018-06-25 21:57:30 -0500

    initial commit

---
+++ dead-center.js
@@ -0,0 +1,37 @@
+var dcvs = [];
+
+
+function DeadCenter(el) {
+  console.log("Createing Dead Center");
+  this.Node = el;
+  this.Parent = this.Node.parentNode;
+  this.Height = this.Node.offsetHeight;
+  this.Offset = calcOffset(this);
+  this.Node.style.top = this.Offset + 'px';
+  this.Node.style.position = 'absolute';
+  this.Node.style.textAlign = 'right'
+  this.Node.style.right = '0px';
+
+  function calcOffset(obj) {
+    var parentHeight = obj.Parent.offsetHeight;
+    var objHeight = obj.Node.offsetHeight;
+    console.log("Parent Height -> " + parentHeight);
+    console.log("Object Height -> " + objHeight);
+    return (parentHeight - objHeight) * 0.25;
+  }
+}
+
+DeadCenter.prototype.Dump = function() {
+  console.log(this);
+}
+
+function deadCenter() {
+  var dcvEls = document.querySelectorAll('.dc-v, .dcv');
+  dcvEls.forEach(function(dcvEl) {
+    console.log(dcvEl);
+    var newDc = new DeadCenter(dcvEl);
+    dcvs.push(newDc);
+  });
+}
+
+deadCenter();
