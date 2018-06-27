bdc83c142ccf0f4845b6041813e5c5f2c66d352f
Jacob Scott <jbscottok@gmail.com>
2018-06-27 00:27:50 -0500

    factor margins into offsets

--- dead-center.js
+++ dead-center.js
@@ -8,19 +8,21 @@ function DeadCenter(el) {
   this.Height = this.Node.offsetHeight;
   this.Offset = calcOffset(this);
   this.Node.style.top = this.Offset + 'px';
-  this.Node.style.position = 'absolute';
+  this.Node.style.position = 'relative';
 
   function calcOffset(obj) {
     var parentHeight = obj.Parent.offsetHeight;
     var objHeight = obj.Node.offsetHeight;
-    console.log("Parent Height -> " + parentHeight);
-    console.log("Object Height -> " + objHeight);
+
+    // Include margins height
+    var style = getComputedStyle(obj.Node);
+    var marginTop = parseInt(style.getPropertyValue('margin-top'));
+    var marginBottom = parseInt(style.getPropertyValue('margin-bottom'));
+    objHeight += marginTop + marginBottom; // Append margin height
+
+    // Return full offset value
     return (parentHeight * 0.5) - (objHeight * 0.5);
   }
-}
-
-DeadCenter.prototype.Dump = function() {
-  console.log(this);
 }
 
 function deadCenter() {
